c099ff317c59cf466972eb09db732e5920bb8e60
rubin <rubin666666666@gmail.com>
2026-01-21 16:33:58 +0200

    Fix file structure: move main.js to root and fix import paths

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { getImagesByQuery, PER_PAGE } from './js/pixabay-api.js';
+import { getImagesByQuery, PER_PAGE } from './src/js/pixabay-api.js';
 import {
   createGallery,
   clearGallery,
@@ -11,7 +11,7 @@ import {
   hideLoadMoreButton,
   disableLoadMoreButton,
   enableLoadMoreButton,
-} from './js/render-functions.js';
+} from './src/js/render-functions.js';
 
 const form = document.querySelector('.search-form');
 const loadMoreBtn = document.querySelector('.load-more');
