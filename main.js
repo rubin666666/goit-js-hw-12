eaed776a21e04998fe9f1e77d13b52bbd78aa68a
rubin <rubin666666666@gmail.com>
2026-01-21 16:52:26 +0200

    Fix izitoast import path

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import iziToast from 'izitoast';
+import iziToast from 'izitoast/dist/js/izitoast.min.js';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery, PER_PAGE } from './src/js/pixabay-api.js';
